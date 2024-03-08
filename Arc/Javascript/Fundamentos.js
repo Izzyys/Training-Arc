e0de19c3ef824f67c08709061ead120c4bb2c7f2
Izzy <yjassan28@gmail.com>
2024-03-07 19:54:24 -0500

    Js

---
+++ Arc/Javascript/Fundamentos.js
@@ -0,0 +1,62 @@
+//Comentarios de una linea
+
+/*Comentario 
+De varias 
+lineas
+*/
+
+/*console.log("Msj console")
+document.write("Msj document")
+alert("Hey")
+*/
+
+// Variables
+
+/*saludo = "Hey"
+console.log(saludo)
+var saludo2 = "holas"
+console.log(saludo2)
+const saludo3 = "holi"
+let saludo4 = "holiss"
+console.log(saludo3)
+console.log(saludo4)
+
+{let local ="Let local"
+console.log(local)
+}
+*/
+
+/*let numero1 = 1
+const numero2 = 2
+
+console.log(numero1)
+console.log(numero2)
+*/
+/*
+Datos primitivos
+-Strings
+""
+''
+``
+se varia para poder poner mensajes con comillas 
+dobles o de una dentro de los textos 'sexo "sudor" y calor'
+
+-Numeros y se usa ; 
+10;
+se puede usar decimales
+9.5;
+
+0xFF;
+
+123e5;
+-Boolean
+true;
+false;
+-Undefined valores con el operador 
+JS pone por defecto el valor o texto undefined
+*/
+
+/*use strict
+activar para usar si o si valores como let o const
+*/
+
